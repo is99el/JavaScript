04b82798c3ed06be14eb546a2aeb8a36e2083afb
ismael <ismaelazaaouaj2004@gmail.com>
2022-10-11 14:25:35 +0200

    Basic

--- Week 1/Basic/script.js
+++ Week 1/Basic/script.js
@@ -1,8 +1,109 @@
-var firstName = "Ismael";
-console.log(firstName);
+// /************************************************
+//  * Variables and data types
+//  */
 
-var lastName = "Azaaouaj";
-var age = 18;
+// var firstName = "Ismael";
+// console.log(firstName);
 
-var fullAge = true;
-console.log(fullAge);
+// var lastName = "Azaaouaj";
+// var age = 18;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = "Software Developer";
+// console.log(job);
+
+// //Variable naming rules
+// var _3years = 3;
+// var ismaelAzaaouaj = " ISSI and ISSI";
+// // var if = 18;
+
+// /*************************************8888888888
+//  * Variable mutation and type coercion
+//  */
+
+// var firstName = "ismael";
+// var age = 28;
+
+// console.log(firstName + " " + age);
+
+// var job, isMarried;
+// job = "software developer";
+// isMarried = false;
+
+// console.log(firstName + " " + "is a " + job + "Is he married? " + isMarried);
+
+// //Variable mutation
+// age = "twenty eight";
+// job = "Driver";
+// alert(firstName + " " + "is a " + job + " Is he married? " + isMarried);
+
+// var lastName = prompt("What is his last Name?");
+// console.log(firstName + " " + lastName);
+
+/***********************************************88
+ * Basic operators
+//  */
+// var year, yearIsmael, yearBurak;
+// now = 2022;
+// ageIsmael = 18;
+// ageBurak = 21;
+
+// //Math operators
+// yearIsmael = now - 18;
+// yearBurak = now - 21;
+// console.log(yearIsmael);
+// console.log(yearBurak);
+
+// console.log(now + 3);
+// console.log(now * 3);
+// console.log(now / 3);
+
+// // Logical operators
+// var burakOlder = ageBurak > ageIsmael;
+// console.log(burakOlder);
+
+// // typeof operator
+// console.log(typeof burakOlder);
+// console.log(typeof ageIsmael);
+// console.log(typeof "Ismael");
+// var x;
+// console.log(typeof x);
+
+// /***********************************
+//  * Operator precedence
+//  */
+
+// var now = 2022;
+// var yearIsmael = 2004;
+// var fullAge = 18;
+
+// //Multiple operators
+// var isFullAge = now - yearIsmael >= fullAge;
+// console.log(isFullAge);
+
+// //Group
+// var ageIsmael = (now = yearIsmael);
+// var ageIsmael = 18;
+// var avarage = ageIsmael + ageBurak / 2;
+// console.log(avarage);
+
+// //Multiple assigments
+// var x, y;
+// x = y = (3 + 5) * 4 - 6;
+// console.log(x, y);
+
+// //More operators
+
+// x = x * 2;
+// console.log(x);
+
+// x++;
+// console.log(x);
+
+// x--;
+// console.log(x);
