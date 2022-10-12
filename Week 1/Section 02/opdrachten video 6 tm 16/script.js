e15aa3115edf08ca5e91ece49148cc63bcd24dc6
ismael <ismaelazaaouaj2004@gmail.com>
2022-10-12 13:23:50 +0200

    Week 1
    opd video 6 t/m 16

---
+++ Week 1/Section 02/opdrachten video 6 tm 16/script.js
@@ -0,0 +1,89 @@
+var huidigeschool = "ROC ALMERE BUITEN";
+var vorigeSchool = "De Meergronden";
+var diploma = "Mavo";
+
+console.log(huidigeschool, vorigeSchool, diploma);
+
+var age = 18;
+console.log(age);
+
+/************
+ * 3 String
+ * 4 number
+ * 5 String
+ * 6 Boolean
+ * 7 String
+ * 8 String
+ */
+
+/* 9 naam en achternaam*/
+const firstName = "Jorn";
+const lastName = "Schmitz";
+
+/*10*/
+
+const rabbit = typeof "konijn";
+console.log(rabbit);
+
+/*11,12,13,14*/
+
+var myAge = 2022 - 2004;
+console.log(myAge);
+
+var myAge = (2022 - 2004) * 2;
+console.log(myAge);
+
+var myAge = (2022 - 2004) ^ 2;
+console.log(myAge);
+
+/*15*/
+var street = "Hengelostraat";
+var number = 25;
+console.log(street, number);
+
+/*16,17*/
+var numberOne = 12 + 15;
+console.log(numberOne);
+var numberOne = 12;
+console.log(numberOne + 15);
+
+/*18,19*/
+var numberOne = 12 + 1;
+var numberTwo = 12;
+x = 1;
+var numberThree = 12 + x;
+
+console.log(numberOne, numberTwo + 1, numberThree);
+
+var numberOne = 12 - 1;
+var numberTwo = 12;
+x = 1;
+var numberThree = 12 - x;
+
+console.log(numberOne, numberTwo - 1, numberThree);
+
+/*20,21*/
+var heightVader = 179;
+var heightMoeder = 160;
+
+var heightKind = (heightVader * 3 + heightMoeder) / 4;
+console.log(heightKind);
+
+var heightVader = 192;
+var heightMoeder = 153;
+
+var heightKind = (heightMoeder * 3 + heightVader) / 4;
+console.log(heightKind);
+
+/*22,23*/
+var BMI = 28.6;
+var gewiht = 93;
+
+var lengte = (93 / 28.6) * 4;
+console.log(lengte);
+
+var BMI = 28.6;
+var lengte = 188;
+
+var gewiht = (188 / 188) * 26.6;
+console.log(gewiht);
