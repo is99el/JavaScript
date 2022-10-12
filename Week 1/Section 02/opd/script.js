995442b849f77be02622cc1ad2409b9b986101da
ismael <ismaelazaaouaj2004@gmail.com>
2022-10-12 11:12:25 +0200

    Week 1
    Section 02

---
+++ Week 1/Section 02/opd/script.js
@@ -0,0 +1,17 @@
+var huidigeschool = "ROC ALMERE BUITEN";
+var vorigeSchool = "De Meergronden";
+var diploma = "Mavo";
+
+console.log(huidigeschool, vorigeSchool, diploma);
+
+var age = 18;
+console.log(age);
+
+/************
+ * 3 String
+ * 4 number
+ * 5 String
+ * 6 Boolean
+ * 7 String
+ * 8 String
+ */
