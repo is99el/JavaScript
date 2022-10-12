1280794a8b99cea2645b86a855e66fcfad007b82
ismael <ismaelazaaouaj2004@gmail.com>
2022-10-12 13:57:20 +0200

    Week 1
    opd video 17 t/m 19

---
+++ Week 1/Section 02/opdrachten video 17 tm 19/script.js
@@ -0,0 +1,72 @@
+/*1*/
+var firstName = "Ismael";
+var lastName = "Azaaouaj";
+var age = 18;
+
+console.log(firstName, lastName, age);
+
+/*2*/
+var firstName = "Ismael";
+console.log("Mijn naam is " + firstName);
+
+/*3*/
+var firstName = "Ismael";
+var lastName = "Azaaouaj";
+
+console.log("Mijn volledige naam is " + firstName, lastName);
+
+/*4*/
+
+var age = "18";
+console.log("In 2056 ben ", age);
+
+/*5*/
+var firstName = "Ismael";
+var lastName = "Azaaouaj";
+var age = 18;
+
+console.log(
+  "Mijn naam is " + firstName,
+  "en ik ben " + age,
+  "jaad oud. ",
+  "Daarnaast is  mijn familienaam ",
+  lastName
+);
+
+/*6*/
+const weer = "Regen";
+console.log("wat ik doe log ik naar de console.");
+if (weer === "Regen") {
+  console.log("Als het regent, dan neem ik mijn paraplu mee naar buiten");
+} else {
+  console.log("Anders zet ik mijn zonnebril op.");
+}
+
+/*7*/
+const kleur = "Geel";
+console.log("wat ik doe log ik naar de console.");
+if (kleur === "geel") {
+  console.log("Als het gras geel is, dan geef ik het water.");
+} else {
+  console.log("Anders doe ik niks");
+}
+
+/*8*/
+const winter = "Winter";
+console.log("wat ik doe log ik naar de console.");
+if (winter === "Winter") {
+  console.log("Als het winter is, dan zet ik de verwarming aan");
+} else {
+  console.log("Anders dans ik op de tafel.");
+}
+
+/*9*/
+const weeer = "Regen";
+console.log("wat ik doe log ik naar de console.");
+if (weeer === "Regen") {
+  console.log(
+    "Als het regent, dan doe ik als het keihard regent een regenpak aan, anders neem ik een paraplu mee."
+  );
+} else {
+  console.log("Als het helemaal niet regent, dan zet ik mijn zonnebril op..");
+}
