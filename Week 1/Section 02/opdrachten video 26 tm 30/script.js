73769aac721cd7c462e0276b4cf6248639f521f4
ismael <ismaelazaaouaj2004@gmail.com>
2022-10-25 11:02:29 +0200

    Week 1
    vid 26 t/m 30

---
+++ Week 1/Section 02/opdrachten video 26 tm 30/script.js
@@ -0,0 +1,31 @@
+/*1,2*/
+let dag = "maandag";
+let tijd = 19;
+
+switch (dag) {
+  case "maandag":
+    if (tijd >= 19) {
+      console.log("falafel met salade en kalkoen");
+    } else {
+      console.log("falafel met salade");
+    }
+    break;
+  case "dinsdag":
+    console.log("spinazie");
+    break;
+  case "woensdag":
+    console.log(" spinazie");
+    break;
+  case "donderdag":
+    console.log(" falafel met salade");
+    break;
+  case "vrijdag":
+    console.log(" falafel met salade");
+    break;
+  case "zaterdag":
+    console.log("linzensoep ");
+    break;
+  case "zondag":
+    console.log("linzensoep");
+    break;
+}
