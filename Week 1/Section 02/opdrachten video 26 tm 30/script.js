4ce65d2ac9a5f1819f65b1f39bb38c3d2b249894
ismael <ismaelazaaouaj2004@gmail.com>
2022-10-25 13:37:45 +0200

    Week 1
    vid 26 t/m 30 klaar

--- Week 1/Section 02/opdrachten video 26 tm 30/script.js
+++ Week 1/Section 02/opdrachten video 26 tm 30/script.js
@@ -1,31 +1,177 @@
-/*1,2*/
-let dag = "maandag";
-let tijd = 19;
+// /*1,2*/
+// let dag = "maandag";
+// let tijd = 19;
 
-switch (dag) {
-  case "maandag":
-    if (tijd >= 19) {
-      console.log("falafel met salade en kalkoen");
-    } else {
-      console.log("falafel met salade");
-    }
-    break;
-  case "dinsdag":
-    console.log("spinazie");
-    break;
-  case "woensdag":
-    console.log(" spinazie");
-    break;
-  case "donderdag":
-    console.log(" falafel met salade");
-    break;
-  case "vrijdag":
-    console.log(" falafel met salade");
-    break;
-  case "zaterdag":
-    console.log("linzensoep ");
-    break;
-  case "zondag":
-    console.log("linzensoep");
-    break;
+// switch (dag) {
+//   case "maandag":
+//     if (tijd >= 19) {
+//       console.log("falafel met salade en kalkoen");
+//     } else {
+//       console.log("falafel met salade");
+//     }
+//     break;
+//   case "dinsdag":
+//     console.log("spinazie");
+//     break;
+//   case "woensdag":
+//     console.log(" spinazie");
+//     break;
+//   case "donderdag":
+//     console.log(" falafel met salade");
+//     break;
+//   case "vrijdag":
+//     console.log(" falafel met salade");
+//     break;
+//   case "zaterdag":
+//     console.log("linzensoep ");
+//     break;
+//   case "zondag":
+//     console.log("linzensoep");
+//     break;
+// }
+
+// /*3*/
+
+// var car = "zebi";
+
+// if (car == "audi") {
+//   console.log("Great!");
+// } else if (car == "mercedes") {
+//   console.log("Bit great");
+// } else if (car == "bmw") {
+//   console.log("Hmmm..");
+// } else if (car == "opel") {
+//   console.log("Ok?..");
+// } else {
+//   console.log("Not so great!");
+// }
+
+// /*4*/
+
+// var car = "audi" || car= "porsche";
+
+// if (car == "audi" || "porsche") {
+//   console.log("Great!");
+// } else if (car == "mercedes") {
+//   console.log("Bit great");
+// } else if (car == "bmw") {
+//   console.log("Hmmm..");
+// } else if (car == "opel") {
+//   console.log("Ok?..");
+// } else {
+//   console.log("Not so great!");
+// }
+
+/*5*/
+
+// var car = "";
+
+// if (car == "audi" || car == "porsche") {
+//   console.log("Great!");
+// } else if (car == "mercedes") {
+//   console.log("Bit great");
+// } else if (car == "bmw") {
+//   console.log("Hmmm..");
+// } else {
+//   console.log("Not so great!");
+// }
+
+// /*6*/
+
+// let job = "teacher";
+
+// switch (job) {
+//   case "Developer":
+//     console.log("Like");
+//     break;
+//   case "Developer"
+//   case "Consultant":
+//     console.log("Nice...");
+//     break;
+//   default:
+//     console.log("I dont know...");
+// }
+
+// /*7*/
+
+// let job = "Consultant";
+
+// switch (job) {
+//   case "Developer":
+//   case "Retired":
+//     console.log("Like");
+//     break;
+//   case "Teacher":
+//   case "Consultant":
+//     console.log("Nice...");
+//     break;
+//   default:
+//     console.log("I dont know...");
+// }
+
+// /*8/
+// let day = "maandag";
+// let message;
+
+// message = day == "maandag" ? "ik eet patat" : "anders eet ik pannenkoek";
+// console.log(message);
+
+// /*9/
+// var car = "audi";
+
+// if (car == "audi") {
+//   console.log("great!");
+// } else {
+//   console.log("Meh");
+// }
+
+/*10/
+
+let sentence = 15;
+
+if (sentence > 12) {
+  console.log("Whiee");
+} else {
+  console.log("Whaaaa");
 }
+
+// /*11*/
+
+// let sentence;
+// sentence = false;
+
+// if (sentence == false) {
+//   console.log("Whiee");
+// } else {
+//   console.log("Whaaa");
+// }
+
+/*13*/
+
+// const city = "Amsterdam";
+// let message;
+
+// message =
+//   city === "Amsterdam"
+//     ? "Koetjekoe"
+//     : `Deze prachtige stad is het niet: ${city}`;
+
+// console.log(message);
+
+/*14*/
+
+// const city = "Amsterdam";
+// let message;
+
+// message = city === "Amsterdam" ? "Koetjekoe" : "Koetjekoe";
+
+// console.log(message);
+
+/*15*/
+
+// const city = "Amsterdam";
+// let message;
+
+// message = city === "Amsterdam" && true ? "Koetjekoe" : "Koetjebeoboe";
+
+// console.log(message);
