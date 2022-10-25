034f25f04651fe3d96f624a5d4aacda4953c964f
ismael <ismaelazaaouaj2004@gmail.com>
2022-10-25 13:54:15 +0200

    Week 1
    ontwikkelaarsopdrachten klaar(week1 af)

---
+++ Week 1/Section 02/ontwikkelaarsopdrachten/script.js
@@ -0,0 +1,26 @@
+/*1*/
+// const age = 18;
+// const birthYear = 1988;
+// switch (true) {
+//   case getResult(age, birthYear):
+//     console.log("creat!");
+//     break;
+//   default:
+//     console.log("vos!");
+// }
+// function getResult(age, birthYear) {
+//   return age >= 18 && String(birthYear).includes("88");
+// }
+
+// omdat de leeftijd groter of gelijk is aan 18 en het geboorte jaar nummer 88 heeft
+
+// /*2*/
+
+// const age = 13;
+// if (age >= 18) {
+//   console.log("Great!");
+// } else if (age >= 12) {
+//   console.log("Bit great!");
+// } else {
+//   console.log("Not so great");
+// }
